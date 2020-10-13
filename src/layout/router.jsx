ae177d677dbabe0ca48bdf9831f9ee45a3e593c6
Andrew Long <drewlongdesign@gmail.com>
2020-10-13 17:18:39 +0000

    Pushing layout

---
+++ src/layout/router.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter, Route, Redirect} from 'react-router-dom'
+import Cookie from 'universal-cookie'
+
+// Route Components
+import Auth from './auth/main.jsx'
+import Dashboard from './home/main.jsx'
+
+const cookie = new Cookie()
+
+export default class Router extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      auth: false
+    }
+  }
+  render(){
+    return(
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={Auth} />
+            <Route exact path="/dashboard" render={(props) => (
+              cookie.get("fp_token") ? (
+                <Dashboard {...props} />
+              ) : (
+                <Redirect to="/"/>
+              )
+            )}/>
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
