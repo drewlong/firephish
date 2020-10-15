830e9ace6fe50468d20d62eca51ba136e2ac8d32
Andrew Long <drewlongdesign@gmail.com>
2020-10-15 04:35:20 +0000

    changes to template editor

--- src/layout/home/components/templates.jsx
+++ src/layout/home/components/templates.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Axios from 'axios'
-import {Button, Divider, Dropdown, Icon, Input, Label, Menu, Message} from 'semantic-ui-react'
-import ReactPrismEditor from "react-prism-editor"
+import {Button, Checkbox, Divider, Dropdown, Icon, Input, Label, Menu, Message, Segment} from 'semantic-ui-react'
 import { Sandbox, withDependencies } from "react-sandbox-editor"
 import config from '../../global/config.json'
 
@@ -11,25 +10,29 @@ export default class Templates extends Component{
   constructor(props){
     super(props)
     this.state = {
-      editor: true,
+      editor: false,
       code: "<h1>Edit HTML here!</h1>",
-      editor_dark_mode: false,
       template_vars: [
         { key: 'tracking', icon: 'chain', text: 'Tracking URL', value: '%%Tracking_URL%%' },
         { key: 'fname', icon: 'user', text: 'First Name', value: '%%First_Name%%' },
         { key: 'lname', icon: 'user', text: 'Last Name', value: '%%Last_Name%%' },
         { key: 'position', icon: 'user', text: 'Position', value: '%%Position%%' },
-        { key: 'group', icon: 'group', text: 'Group Name', value: '%%Group_Name%%' },
-
-      ]
+        { key: 'group', icon: 'group', text: 'Group Name', value: '%%Group_Name%%' }
+      ],
+      cb_fail: false
     }
   }
   handleTempVar = (e, {value}) => {
-    this.setState({temp_var: value, copied: true})
+    this.setState({temp_var: value})
     setTimeout(() => {
       this.setState({copied: false})
     }, 1500)
-    navigator.clipboard.writeText(value)
+    if(navigator.clipboard){
+      navigator.clipboard.writeText(value).then((r) => {}).catch((e) => {})
+      this.setState({copied: true})
+    }else{
+      this.setState({cb_fail: true})
+    }
   }
   handleCopyTempVar = () => {
     this.setState({copied: true})
@@ -37,9 +40,6 @@ export default class Templates extends Component{
       this.setState({copied: false})
     }, 1500)
     navigator.clipboard.writeText(this.state.temp_var)
-  }
-  handleInsertTempVar = () => {
-
   }
   render(){
     return(
@@ -51,30 +51,36 @@ export default class Templates extends Component{
         {this.state.editor &&
           <div className="column">
             <div className="row">
-              <div style={{flex: 3}}>
+              <div style={{flex: 1}}>
                 <h3>Template Editor</h3>
+              </div>
+              <div style={{flex: 1}}>
+                <Button color="grey">Dark Mode</Button>
               </div>
               <div style={{flex: 1}}>
                 <div className="row">
                   <Dropdown
-                    placeholder='Template Variables'
+                    fluid
+                    placeholder='Select Template Variable'
                     search selection options={this.state.template_vars}
                     onChange={this.handleTempVar}
                     />
                   <span>&nbsp;</span>
-                  {this.state.temp_var &&
+                  {this.state.temp_var && !this.state.cb_fail &&
                     <Button icon="copy" color="teal" onClick={this.handleCopyTempVar}/>
                   }
                 </div>
               </div>
               <div style={{flex: 1}}>
-                <div className="row">
                   {this.state.copied &&
-                    <Message size="mini" color="green">Copied to clipboard</Message>
+                    <div className="row">
+                      <Message size="mini" color="green">Copied to clipboard</Message>
+                    </div>
+                  }{this.state.cb_fail && this.state.temp_var &&
+                    <Input fluid value={this.state.temp_var} />
                   }
-                </div>
               </div>
-              <div style={{paddingRight: 20}}>
+              <div style={{padding: "0 20px 0 5px"}}>
                 <Button.Group>
                   <Button basic color="green">Save</Button>
                   <Button basic color="red">Cancel</Button>
@@ -93,6 +99,8 @@ export default class Templates extends Component{
                     readOnly: false,
                     wrapLines: false
                   }}
+                  scriptEditor={{readOnly: true}}
+                  stylesheetEditor={{readOnly: true}}
                   executeOnCodeChange={true}
                   onCodeChange={(name, val) => {this.setState({code: val})}}
                 />
